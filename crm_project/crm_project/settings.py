6d7c3e1cb677b5f2d4df1c004872382f57275490
João França <joao.francapf@gmail.com>
2020-06-21 16:02:52 -0300

    Uptaded settings

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
