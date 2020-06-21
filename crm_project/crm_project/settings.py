21e591bee9187eac2cfa5b2bfd31dad178f94f4b
João França <joao.francapf@gmail.com>
2020-06-21 15:19:20 -0300

    Uptaded settings

--- crm_project/crm_project/settings.py
+++ crm_project/crm_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['crmfacil.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
